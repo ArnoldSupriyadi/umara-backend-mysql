3ff827009ecbec347ef7725495c66bb2093f9101
ArnoldSupriyadi <arnoldsupriyadi@gmail.com>
2026-05-02 20:36:53 +0700

    testing workflows

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -11,7 +11,7 @@ class UserSeeder extends Seeder
 {
     public function run(): void
     {
-        // pastikan role ada
+        // pastikan role ada ya
         $role = Role::firstOrCreate([
             'name' => 'super_admin',
             'guard_name' => 'web'
